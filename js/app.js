e98ca70d274a69ea6b962ddd4d8d8d4a3ffb85c8
Hasanat902 <abulhasanat902@gmail.com>
2022-02-28 16:21:56 +0600

    added player details

--- js/app.js
+++ js/app.js
@@ -15,7 +15,6 @@ const displayPlayer = players => {
     playersContainer.textContent = '';
 
     players.forEach(player => {
-        console.log(player);
         const div = document.createElement("div");
         div.classList.add("card");
         div.classList.add("mb-5");
@@ -25,9 +24,36 @@ const displayPlayer = players => {
                 <h5 class="card-title">Name: ${player.strPlayer}</h5>
                 <h6 class="card-title">Country: ${player.strNationality}</h6>
                 <p class="card-text">Position: ${player.strPosition}</p>
-                <a href="#" class="btn btn-primary">Details</a>
+                <a onclick="loadPlayerDetails('${player.idPlayer}')" href="#" class="btn btn-primary">Details</a>
             </div>
         `;
         playersContainer.appendChild(div);
     })
+}
+
+const loadPlayerDetails = playerId => {
+    
+    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${playerId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPlayerDetails(data.players[0]))
+}
+
+const displayPlayerDetails = details => {
+    const playerDetails = document.getElementById("player-details");
+
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+        <img src="${details.strThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h4 class="card-title">Name: ${details.strPlayer}</h4>
+            <h5 class="card-title">Birthday: ${details.dateBorn}</h5>
+            <h5 class="card-title">Country: ${details.strNationality}</h5>
+            <h6 class="card-text">Position: ${details.strPosition}</h6>
+            <p class="card-text">Description: ${details.strDescriptionEN.slice(0, 300)}</p>
+        </div>
+    `;
+
+    playerDetails.appendChild(div);
 }
